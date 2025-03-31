94d3135567bed251a4f98c52bfdd9086211b4f00
unibox <unibox@duck.com>
2025-03-31 12:33:06 +0200

    some style fixe

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import svelte from '@astrojs/svelte';
 export default defineConfig({
   integrations: [svelte()],
   site: 'https://uniboxx.github.io',
-  base: '/astro-landing-book',
+  base: '/astro-sell-one',
 });
